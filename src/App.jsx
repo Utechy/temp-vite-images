418604e2dca40e58999c18f35dde0af68e2afa2a
Utsav Kumar <utsavkumarapp@gmail.com>
2023-12-23 18:45:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
+
+// API Key- 8mmo6NnebXw43QOuQzP9RS2dlN6g31A5ivLTtGZrbXo
